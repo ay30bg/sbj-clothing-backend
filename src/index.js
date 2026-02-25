65f3ed7ee9c279aa184b6b68ac31ce109f6db515
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-25 16:43:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch((err) => console.log(err));
-
-// app.use("/api/auth", require("./routes/authRoutes"));
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
@@ -51,3 +28,4 @@ mongoose
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
