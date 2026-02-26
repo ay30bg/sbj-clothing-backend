e00b8b8af8de25e84173e3ae64460a8d031b74cd
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-26 05:30:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
 
 // Connect DB
 mongoose
@@ -28,4 +29,5 @@ mongoose
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
