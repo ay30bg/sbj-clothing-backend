62afecc47b86ecea0f313e72725069147f6a23b5
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-28 11:01:07 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -155,7 +155,7 @@ app.use('/api/orders', orderRoutes);
 
 // ================= MONGODB =================
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB Connected'))
   .catch((err) =>
     console.error('❌ MongoDB Connection Error:', err.message)
@@ -181,6 +181,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
 
 
